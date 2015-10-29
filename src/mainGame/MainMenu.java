35f02cd330164e42d7053711c1119f24ae8ec834
NValentin <NValentin@users.noreply.github.com>
2015-10-29 11:36:09 +0100

    Fixed to work with IntelliJ

---
+++ src/mainGame/MainMenu.java
@@ -0,0 +1,14 @@
+package mainGame;
+
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.SlickException;
+
+public class MainMenu
+{
+	void render(GameContainer gc, Graphics g) throws SlickException
+	{
+		
+	}
+
+}

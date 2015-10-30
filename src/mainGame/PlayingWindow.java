a7b838d081ca8bce4de755acac93d6073719749d
Pkingo <pkingo14@student.aau.dk>
2015-10-30 14:56:03 +0100

    Added PlayingWindow Class, thats it

---
+++ src/mainGame/PlayingWindow.java
@@ -0,0 +1,35 @@
+package mainGame;
+
+import org.newdawn.slick.BasicGame;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.SlickException;
+
+/**
+ * Created by Kingo on 30-10-2015.
+ */
+public class PlayingWindow extends BasicGame{
+
+    @Override
+    public void init(GameContainer gameContainer) throws SlickException {
+
+    }
+
+    @Override
+    public void update(GameContainer gameContainer, int i) throws SlickException {
+
+    }
+
+    @Override
+    public void render(GameContainer gameContainer, Graphics graphics) throws SlickException {
+
+    }
+
+    public PlayingWindow(String title) {
+        super(title);
+    }
+
+    public static void main(String[] args) {
+
+    }
+}

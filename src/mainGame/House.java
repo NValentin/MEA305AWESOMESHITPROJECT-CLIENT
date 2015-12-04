b3dd8e277e832feed043015eb78016f5653a9bf9
NValentin <NValentin@users.noreply.github.com>
2015-12-04 14:43:06 +0100

    Comments

--- src/mainGame/House.java
+++ src/mainGame/House.java
@@ -5,20 +5,26 @@ import org.newdawn.slick.Graphics;
 import org.newdawn.slick.Image;
 import org.newdawn.slick.geom.Circle;
 
+/**
+ * The house class is used for representing where players have built houses. They are build from circles received
+ * from the housePlots in GameMap
+ */
 public class House
 {
-    public Boolean getIsCity()
-    {
-        return isCity;
-    }
+    //Variables
+    private Boolean isCity; //bool for if the house has been upgraded to a city
+    private int playerID; //the id of the player that built the house
+    private Color buildingColor; //the color of the houses, dependant on the playerID
+    private Circle houseCircle; //the house circle represents the circle on screen space where it gets drawn
 
-    private Boolean isCity;
-    private int playerID;
-    private Color buildingColor;
-    private Circle houseCircle;
-
+    /**
+     * Constructor for the house class, instantiate a new house
+     * @param circle the circle representing the house
+     * @param playerID the int ID representing the players ID (1-4)
+     */
     public House(Circle circle, int playerID)
     {
+        //Initialize a new house variables
         isCity = false;
         this.playerID = playerID;
         houseCircle = circle;
@@ -28,6 +34,11 @@ public class House
 
     }
 
+    /**
+     * How a house is rendered, sets its color, textures it, and resets color to default(white)
+     * @param g graphics component
+     *          @see Graphics and Slick2d javadoc
+     */
     public void render(Graphics g)
     {
         g.setColor(buildingColor);
@@ -35,6 +46,10 @@ public class House
         g.setColor(Color.white);
     }
 
+    /**
+     * Method for finding the texture a house should have. Use one if it's a house, another if it's a city.
+     * @return Image type
+     */
     private Image getTexture()
     {
         if (isCity)
@@ -46,6 +61,9 @@ public class House
         }
     }
 
+    /**
+     * Upgrades a house to a city
+     */
     public void upgradeHouse()
     {
         isCity = true;
@@ -53,11 +71,29 @@ public class House
             PlayerStats.point +=1;
     }
 
+    /**
+     * Determines if a house has been upgraded to a city
+     * @return Boolean
+     */
+    public Boolean getIsCity()
+    {
+        return isCity;
+    }
+
+    /**
+     * Determines a house's playerID
+     * @return int
+     */
     public int getPlayerID()
     {
         return playerID;
     }
 
+    /**
+     * Determines a house's circle
+     * @return Circle
+     * @see Circle and Slick2D javadoc
+     */
     public Circle getHouseCircle()
     {
         return houseCircle;
--- src/mainGame/Road.java
+++ src/mainGame/Road.java
@@ -4,20 +4,34 @@ import org.newdawn.slick.Color;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.geom.Line;
 
+/**
+ * Road class represents roads in the game
+ */
 public class Road
 {
-
+    //Variables belonging to a road
     private Line roadLine;
     private int playerID;
     private Color roadColor;
 
+    /**
+     * Constructor of a road
+     * @param roadLine Line representing the road
+     * @param playerID int representing the ID of the player that built the road
+     */
     public Road(Line roadLine, int playerID)
     {
+        //Instantiate a road's variables
         this.playerID = playerID;
         this.roadLine = roadLine;
         roadColor = PlayerStats.playerColors[playerID];
     }
 
+    /**
+     * A roads render class, draws the road
+     * @param g Graphics component
+     *          @see Graphics and Slick2D javadoc
+     */
     public void render(Graphics g)
     {
         g.setColor(roadColor);
@@ -26,11 +40,20 @@ public class Road
         g.setColor(Color.white);
     }
 
+    /**
+     * Line representing a road's line
+     * @return Line
+     * @see Line
+     */
     public Line getRoadLine()
     {
         return roadLine;
     }
 
+    /**
+     * Int representing the ID of the player that built the houses
+     * @return int
+     */
     public int getPlayerID()
     {
         return playerID;
